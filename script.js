c3d2ed6a2fb57addedd7bde1a6d62a02d403eaca
Jeff Keating <jkeatinco@gmail.com>
2021-01-12 12:25:42 -0800

    range slider value display

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var actualyear = years + 2021
     
+}
+
+function displayRange(val)
+{
+    document.getElementById('spanVal').innerHTML = val; 
 }
         
